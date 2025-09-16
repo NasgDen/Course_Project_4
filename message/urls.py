e1406b32dbba5c82709397abe03f4e0cd34f89f2
Your Name <you@example.com>
2025-09-16 20:11:07 +0500

    В модуль settings.py добавлена конфигурация электронной почты

--- message/urls.py
+++ message/urls.py
@@ -9,7 +9,7 @@ from .views import MailingRecipientListView, MailingRecipientCreateView, Mailing
 app_name = MessageConfig.name
 
 urlpatterns = [
-    path('recipient_list/',MailingRecipientListView.as_view(), name='recipient_list'),
+    path('',MailingRecipientListView.as_view(), name='recipient_list'),
     path('recipient_add/', MailingRecipientCreateView.as_view(), name='recipient_add'),
     path('recipient_update/<int:pk>/', MailingRecipientUpdateView.as_view(), name='recipient_update'),
     path('recipient_delete/<int:pk>/', MailingRecipientDeleteView.as_view(), name='recipient_delete'),
