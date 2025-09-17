7ed7ade2b0df7a45517defa669f652af5279fc0c
Your Name <you@example.com>
2025-09-17 21:28:30 +0500

    Реализована функция - попытка рассылок

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import MailingRecipient, Message, Distribution, AttemptedMailing
 
-# Register your models here.
+@admin.register(MailingRecipient)
+class MailingRecipientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment',)
+    list_filter = ('email',)
+    search_fields = ('email', 'full_name',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'text',)
+    list_filter = ('subject', 'text',)
+    search_fields = ('subject', 'text',)
+
+
+@admin.register(Distribution)
+class DistributionAdmin(admin.ModelAdmin):
+    list_display = ('datatime_first_sending', 'datetime_end_sending', 'status',)
+    list_filter = ('status',)
+    search_fields = ('status',)
+
+
+@admin.register(AttemptedMailing)
+class AttemptedMailingAdmin(admin.ModelAdmin):
+    list_display = ('attempt_datetime', 'status', 'mail_server_response',)
+    list_filter = ('status',)
+    search_fields = ('status',)
