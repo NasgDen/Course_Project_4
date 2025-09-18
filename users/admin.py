a6fd9774b7a1e44aef57806b6741a251869ac36d
Your Name <you@example.com>
2025-09-18 21:31:21 +0500

    Модель CustomUser добавлена в админку

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'avatar', 'country',)
+    list_filter = ('country',)
+    search_fields = ('email', 'phone_number', 'country',)
